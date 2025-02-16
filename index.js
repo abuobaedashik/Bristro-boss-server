e309710fa795667e02cc33f0bfff1e736c7a455f
abuobaedashik <abuobaedashik@gmail.com>
2025-02-16 19:20:16 +0600

    data deldte api create successfully

--- index.js
+++ index.js
@@ -139,6 +139,13 @@ async function run() {
          res.send(result)
       })
 
+    app.delete('/menu/:id',verifyToken,verifyAdmin,async(req,res)=>{
+      const id =req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result =await MenuCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
 
 
